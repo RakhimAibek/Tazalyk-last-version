506923de4cabbc324b23c77b6093b3502edac7a4
Aibek Rakhim <aibekrakhim@aibeks-macbook-pro.local>
2017-07-21 12:27:58 +0600

    Initial Commit

---
+++ Tazalyk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tazalyk
+//
+//  Created by Aibek Rakhim on 7/21/17.
+//  Copyright © 2017 Next Step. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
